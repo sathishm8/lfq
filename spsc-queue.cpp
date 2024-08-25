6e2bb57236e2af69c49a94048e8e609fbdf4b2de
Sathishkumar S <sathish.matb@gmail.com>
2024-08-25 21:49:52 +0530

    spsc lockfree queue
    Tested on AMD Ryzen 7 3700U
    
    Queue Size: 1024
    Producer work cycles k = 10000000
    Queue Time: 1197 ms
    Consumer work cycles k = 10000000
    Dequeue Time: 1197 ms
    
    Queue Size: 1024
    Producer work cycles k = 10000000
    Queue Time: 1343 ms
    Consumer work cycles k = 10000000
    Dequeue Time: 1343 ms
    
    ueue Size: 64
    Producer work cycles k = 10000000
    Queue Time: 2789 ms
    Consumer work cycles k = 10000000
    Dequeue Time: 2789 ms
    
    Queue Size: 64
    Producer work cycles k = 10000000
    Queue Time: 2969 ms
    Consumer work cycles k = 10000000
    Dequeue Time: 2969 ms
    
    Queue Size: 32
    Producer work cycles k = 10000000
    Queue Time: 2834 ms
    Consumer work cycles k = 10000000
    Dequeue Time: 2834 ms
    
    Queue Size: 32
    Producer work cycles k = 10000000
    Queue Time: 3752 ms
    Consumer work cycles k = 10000000
    Dequeue Time: 3752 ms
    
    Signed-off-by: Sathishkumar S <sathish.matb@gmail.com>

---
+++ spsc-queue.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <thread>
+#include <atomic>
+
+using namespace std;
+
+template <typename TT, uint32_t n>
+class ringBuf {
+private:
+	vector<TT>			ring;
+	atomic<uint32_t>	rptr, wptr;
+	atomic<uint32_t>	count;
+	uint32_t			size;
+
+public:
+	ringBuf() : size(n), rptr(0),	wptr(0), count(0), ring(n) {
+		cout << "SPSC Queue Size: " << n << endl;
+	}
+
+	void update_size(uint32_t);
+	bool enqueue(TT);
+	bool dequeue(TT*);
+	uint32_t get_size(void) {
+		return size;
+	}
+	bool ringIsFull(void) {
+		return (count == size);
+	}
+	bool ringIsEmpty(void) {
+		return (count == 0);
+	}
+};
+
+template <typename TT, uint32_t n>
+void ringBuf<TT, n>::update_size(uint32_t s) {
+	size = s;
+	ring.resize(size);
+}
+
+template <typename TT, uint32_t n>
+bool ringBuf<TT, n>::enqueue(TT o)
+{
+	if ((wptr + 1) % size == rptr)
+		return false;
+	ring[wptr] = o;
+	wptr = (wptr + 1) % size;
+	count++;
+	return true;
+}
+
+template <typename TT, uint32_t n>
+bool ringBuf<TT, n>::dequeue(TT* o)
+{
+	if (rptr == wptr)
+		return false;
+	*o = ring[rptr];
+	rptr = (rptr + 1) % size;
+	count--;
+	return true;
+}
+
+typedef std::chrono::high_resolution_clock Clock;
+
+int main(int argc, char* argv[])
+{
+	ringBuf<int*, 1024> r;
+	thread t[2];
+	int d1 = 1, *p;
+
+	auto pwork = [&r](int *addr, int n) {
+		int k = 0;
+		auto start_time = Clock::now();
+		do {
+			if (!r.enqueue(addr))
+				std::this_thread::sleep_for(2ns);
+			else
+				k++;
+		} while (k < n);
+		auto end_time = Clock::now();
+		cout << "Producer work cycles = " << k << " " << *addr << endl;
+	    cout << "Queue Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time    - start_time).count() << " ms" << std::endl;
+		std::this_thread::sleep_for(20ms);
+	};
+
+	auto cwork = [&r](int **p, int n) {
+		int k = 0;
+		auto start_time = Clock::now();
+		do {
+			if (!r.dequeue(p))
+				std::this_thread::sleep_for(4ns);
+			else
+				k++;
+		} while (k < n);
+		auto end_time = Clock::now();
+		std::this_thread::sleep_for(20ms);
+		cout << "Consumer work cycles = " << k << " " << **p << endl;
+	    cout << "Dequeue Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time    - start_time).count() << " ms" << std::endl;
+	};
+
+	t[0] = std::thread(pwork, &d1, 10*1000*1000);
+	t[1] = std::thread(cwork, &p, 10*1000*1000);
+	t[0].join();
+	t[1].join();
+
+	return 0;
+}
+
